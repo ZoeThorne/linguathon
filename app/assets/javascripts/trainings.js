8a0524364fcd86fe8d6366c05f6a13a9e33f3240
Zoe Thorne <zathorne@gmail.com>
2016-12-01 13:51:03 +0100

    Add hardcoded version of trainings.js

---
+++ app/assets/javascripts/trainings.js
@@ -0,0 +1,42 @@
+function getWordsFromAjax(){
+	console.log("JS linked OK")
+	// var id = "#{@training.id}";
+	console.log(id)
+	$.ajax({
+		type: "GET", 
+		url: "/api/trainings/"+id+"/words",
+		success: showFirstQuestion, 
+		error: handleError
+	});
+};
+
+function showFirstQuestion(response){
+	console.log("It worked");
+	console.log(response[0]);
+	$('.training-question-1').empty();
+	var q = `<h1>${response[0].english}</h1>
+				<br><h3>What is the German?</h3>`;
+	$('.training-question-1').append(q)
+
+};
+
+function getFirstAnswer(){
+	$('.answer-question-1').submit(function(event){
+		event.preventDefault();
+		var guess1 = $('#q1-answer').val();
+		console.log(guess1)
+		$('.training-1').hide();
+		$('.training-2').show();
+	});
+};
+
+
+function handleError(error){
+	console.log("Error");
+	console.log(error.responseText); 
+}
+
+$(document).on('ready', function () {
+	getWordsFromAjax();
+	getFirstAnswer();
+});
