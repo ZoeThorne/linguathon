8438b27e43a448c0105b3402d4dbefa5d50869cd
Zoe Thorne <zathorne@gmail.com>
2016-12-01 18:26:12 +0100

    Add AJAX method to check answer

--- app/assets/javascripts/trainings.js
+++ app/assets/javascripts/trainings.js
@@ -1,34 +1,100 @@
 function getWordsFromAjax(){
-	console.log("JS linked OK")
 	// var id = "#{@training.id}";
 	console.log(id)
 	$.ajax({
 		type: "GET", 
 		url: "/api/trainings/"+id+"/words",
-		success: showFirstQuestion, 
+		success: generateQuestions, 
 		error: handleError
 	});
 };
 
-// function showFirstQuestion(response){
-// 	console.log("It worked");
-// 	console.log(response[0]);
-// 	$('.training-question-1').empty();
-// 	var q = `<h1>${response[0].english}</h1>
-// 				<br><h3>What is the German?</h3>`;
-// 	$('.training-question-1').append(q)
+function generateQuestions(response){
+	$( "[class*=training-question]" ).each(function( index ) {
+	  $( this ).empty();
+	  var questionNumber = index + 1;
+	  var q = `
+			<h1>${questionNumber}. &nbsp; <strong>${response[index].english}</strong></h1>
+			<br><h3>What is the German?</h3>
+	  		`;
+	  $( this ).append(q);
+	});
 
+	$('.js-answer').each(function(index) {
+		$(this).attr("id",response[index].id);
+	});
+	// var totalQuestions = 10;
+	// var currentQuestion = 0;
+	// var $questions = $(".training-question");
+	// console.log($questions);
+	// // $($questions.eq(currentQuestion)).show();
+	// $(".training-question").eq(currentQuestion).show();
+	// console.log(currentQuestion);
+	// console.log($(".training-question").eq(currentQuestion))
+};
+
+
+// function getAnswer(){
+// 	$("[class*=answer-question]" ).submit(function(event){
+// 		event.preventDefault();
+// 		var guess = $( event.currentTarget ).val();
+// 		console.log(guess)
+// 		// $('.training-1').hide();
+// 		// $('.training-2').show();
+// 	});
 // };
 
 function getFirstAnswer(){
 	$('.answer-question-1').submit(function(event){
 		event.preventDefault();
 		var guess1 = $('#q1-answer').val();
+		var answer1 = $('input:hidden').attr('id');
+		// var answer = $('input[name$="q1-answer"]').attr('id');
+		// var answer = $('.js-q1-answer').attr('id');
 		console.log(guess1)
-		$('.training-1').hide();
-		$('.training-2').show();
+		console.log(answer1)
+		ajaxCheck(guess1, answer1);
 	});
 };
+
+function ajaxCheck(guess1, answer1){;
+	$.ajax({
+		type: "GET",
+		data: guess1,
+		url: "/api/trainings/"+id+"/words/"+answer1+"/check",
+		success: function(response){
+			console.log(response.tl);
+			console.log(guess1);
+			$('#response').empty();
+			if (response.tl == guess1) {
+				$('#response').append("Correct!")
+			} else {
+				$('#response').append("The answer was actually " +response.tl)
+			}
+			var next = `
+				<br>
+				<button id="next" class="next" name="next">Next</button>
+			`
+			$('#response').append(next)
+		}, 
+		error: handleError
+	});
+};
+
+function nextQuestion(){
+	$(document).on('click', '.next', function (event){
+  		event.preventDefault();
+  		$('.training-1').hide();
+		$('.training-2').show();
+		$('#response').empty();
+		console.log("Button clicked");
+	});
+};
+
+// function checkAnswer(response){
+// 	console.log(response);
+// 	// console.log(guess1);
+// };
 
 
 function handleError(error){
@@ -38,5 +104,7 @@ function handleError(error){
 
 $(document).on('ready', function () {
 	getWordsFromAjax();
+	// getAnswer();
 	getFirstAnswer();
+	nextQuestion();
 });
--- app/controllers/api_words_controller.rb
+++ app/controllers/api_words_controller.rb
@@ -10,6 +10,16 @@ class ApiWordsController < ApplicationController
 	end
 
 	def check
+		@user = current_user
+		@training = Training.find_by(id: params[:training_id])
+		@word = Word.find_by(id: params[:word_id])
+		# @guess = guess1
+		render json: @word
+		# render :json => {
+  		#     		:word => @word,
+  		#     		:guess => @guess
+  		#  		}
+
 	end
 
 end
