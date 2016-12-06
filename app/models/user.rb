b9b32753fac2e3e96a2fbec9bc4d9dbeccd79d47
Zoe Thorne <zathorne@gmail.com>
2016-12-06 14:21:50 +0100

    Add word achievements

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,10 +26,10 @@ class User < ApplicationRecord
     end
 
     def check_achievements(user)
-    	new_achievements = []
+    	
 
     	user.streak_achievements(user)
-    	
+    	user.words_achievements(user)
     
 
     end
@@ -42,19 +42,57 @@ class User < ApplicationRecord
     		new_a.save
 
     	elsif streak >= 2 && user.has_achievement?(user, "2days") == false
-    		new_achievements.push("2days")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "2days"))
+    		new_a.save
     	elsif streak >= 3 && user.has_achievement?(user, "3days") == false
-    		new_achievements.push("3days")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "3days"))
+    		new_a.save
     	elsif streak >= 5 && user.has_achievement?(user, "5days") == false
-    		new_achievements.push("5days")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "5days"))
+    		new_a.save
     	elsif streak >= 7 && user.has_achievement?(user, "7days") == false
-    		new_achievements.push("7days")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "7days"))
+    		new_a.save
     	elsif streak >= 14 && user.has_achievement?(user, "2weeks") == false
-    		new_achievements.push("2weeks")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "2weeks"))
+    		new_a.save
     	elsif streak >= 21 && user.has_achievement?(user, "3weeks") == false
-    		new_achievements.push("3weeks")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "3weeks"))
+    		new_a.save
     	elsif streak >= 28 && user.has_achievement?(user, "1month") == false
-    		new_achievements.push("1month")
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "1month"))
+    		new_a.save
     	end
+    end
+
+    def words_achievements(user)
+    	total_words = user.words.length
+
+    	if total_words >= 10 && user.has_achievement?(user, "10words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "10words"))
+    		new_a.save
+    	elsif total_words >= 25 && user.has_achievement?(user, "25words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "25words"))
+    		new_a.save
+    	elsif total_words >= 50 && user.has_achievement?(user, "50words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "50words"))
+    		new_a.save
+    	elsif total_words >= 100 && user.has_achievement?(user, "100words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "100words"))
+    		new_a.save
+    	elsif total_words >= 250 && user.has_achievement?(user, "250words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "250words"))
+    		new_a.save
+    	elsif total_words >= 500 && user.has_achievement?(user, "500words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "500words"))
+    		new_a.save
+    	elsif total_words >= 1000 && user.has_achievement?(user, "1000words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "1000words"))
+    		new_a.save
+    	elsif total_words >= 1500 && user.has_achievement?(user, "1500words") == false
+    		new_a = user.user_achievements.create(achievement: Achievement.find_by(name: "1500words"))
+    		new_a.save	
+    	end
+
     end
 end
